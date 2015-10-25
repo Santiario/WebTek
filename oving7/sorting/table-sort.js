5e22e89f8816b130bfe76140d641546a31c330d4
Vemund Santi <vemund@santi.no>
2015-10-25 14:13:19 +0100

    Added index.html for navigation. Added titles to all HTML files

--- oving7/sorting/table-sort.js
+++ oving7/sorting/table-sort.js
@@ -3,12 +3,12 @@
  */
 
 getTableArray = function(){
-    tbody = document.getElementById('the-table-body');
-    trows = tbody.rows;
-    tableArray = [];
-    for(i = 0; i < trows.length; i++){
+    var tbody = document.getElementById('the-table-body');
+    var trows = tbody.rows;
+    var tableArray = [];
+    for(var i = 0; i < trows.length; i++){
         tableArray[i] = [];
-        for(j = 0; j < trows[i].cells.length; j++){
+        for(var j = 0; j < trows[i].cells.length; j++){
             tableArray[i][j] = trows[i].cells[j].innerHTML;
         }
     }
@@ -16,8 +16,8 @@ getTableArray = function(){
 };
 
 insertNewTable = function(table){
-    trows = document.getElementById('the-table-body').rows;
-    for(i = 0; i < trows.length; i++){
+    var trows = document.getElementById('the-table-body').rows;
+    for(var i = 0; i < trows.length; i++){
         trows[i].innerHTML = '<td>' + table[i].join('</td><td>') + '</td>'
     }
 };
@@ -25,10 +25,8 @@ insertNewTable = function(table){
 sortCol = function(col){
     insertNewTable(getTableArray().sort(function(a, b){
         if(typeof a[col] == 'string'){
-            console.log('sorting strings');
             return a[col].localeCompare(b[col]);
         }
         return a[col] - b[col];
     }));
-
 };
